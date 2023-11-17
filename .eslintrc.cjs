c97ab8ce9a75926075efd17b3e6d7086279e5fdf
Manuchehr <manuchekhr.3232@gmail.com>
2023-11-17 19:43:24 +0500

    feat: add eslint & prettier

---
+++ .eslintrc.cjs
@@ -0,0 +1,41 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:vue/vue3-essential",
+    "prettier",
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [
+        "src/**/*.{vue,js,ts,json}",
+        "vite.config.js",
+        ".eslintrc.{js,cjs}",
+      ],
+      parserOptions: {
+        sourceType: "script",
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    parser: "@typescript-eslint/parser",
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint", "vue"],
+  rules: {
+    // indent: ["error", "tab"],
+    "linebreak-style": ["error", "unix"],
+    quotes: ["error", "double"],
+    semi: ["error", "never"],
+    "no-undef": "off",
+    "@typescript-eslint/no-unused-vars": "warn",
+  },
+}
